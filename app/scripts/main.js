0769453f5250cb0bc75b2a588253ebb4becbfb66
Simon Schmid <simon@schmidsi.ch>
2015-11-06 17:32:28 +0100

    title with font

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,8 +1,20 @@
 /* global $ */
 
+window.devHelper = {
+  overlayStatus: 0 // 0: hidden, 1: 0.5 opaque, 2: visible
+}
+
 window.addEventListener('keypress', (e) => {
   // key §
   if (e.keyCode === 167) {
-    $('#devhelper').toggle()
+    window.devHelper.overlayStatus = (window.devHelper.overlayStatus + 1) % 3
+
+    if (window.devHelper.overlayStatus === 0) {
+      $('#devhelper').hide()
+    } else if (window.devHelper.overlayStatus === 1) {
+      $('#devhelper').show().css('opacity', 0.5)
+    } else if (window.devHelper.overlayStatus === 2){
+      $('#devhelper').show().css('opacity', 1)
+    }
   }
 })
